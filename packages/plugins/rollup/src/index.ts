0675736e1df5dd3f90cbbd8c2f8969a47ae8ecc4
JohnhanLiu <jonhnanliu@gmail.com>
2023-04-11 20:13:17 +0800

    feat(rollup): test

--- packages/plugins/rollup/src/index.ts
+++ packages/plugins/rollup/src/index.ts
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+// console.log(111)
 
 import { dirname, resolve } from 'path'
 
